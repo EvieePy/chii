d5744c9413dca37e1fe83709f01f73e8bff352e0
EvieePy <evieepy@gmail.com>
2024-02-14 16:32:04 +1000

    Remove validator for swagger.

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -4,6 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "/api/schema",
+    validatorUrl: null,
     dom_id: '#swagger-ui',
     supportedSubmitMethods: [],
     deepLinking: true,
